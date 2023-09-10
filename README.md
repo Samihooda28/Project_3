2326c233a48a870f90c230c0446fc7e1b8ce1c0f
VioletRogue12 <mmj649@gmail.com>
2023-09-10 11:38:32 +1000

    ipynb file updated, postgres file updated

--- README.md
+++ README.md
@@ -26,7 +26,11 @@ Datasources:
 - Pie chart total deaths per country - top 10
 
 **References**
-
+ 
 - eumiro, edited by AMC, StackOverflow, 16 Feb 2020, "How to drop rows of Pandas DataFrame whose value in a certain column is NaN", https://stackoverflow.com/questions/13413590/how-to-drop-rows-of-pandas-dataframe-whose-value-in-a-certain-column-is-nan, accessed 9 Sept 2023 
 
 Admin, 28 January 2023, Sparkby{Example}, "Pandas set index name to dataframe", https://sparkbyexamples.com/pandas/pandas-set-index-name-to-dataframe/, accessed 9 September 2023
+
+E Trabelsi, 7 September 2019, "Style Pandas Dataframe Like a Master", https://towardsdatascience.com/style-pandas-dataframe-like-a-master-6b02bf6468b0, accessed 10 September 2023
+
+Talk Saif Malik, 24 August 2022, "How to Alter Column Type in PostgresSQL", https://www.commandprompt.com/education/how-to-alter-column-type-in-postgresql/#:~:text=You%20have%20to%20use%20several,%5D%20TYPE%20modified_data_type%2C%20..., accessed 20 September 2023
--- project_files/project_3_postgres.sql
+++ project_files/project_3_postgres.sql
@@ -4,7 +4,7 @@ DROP TABLE "Country_Data"
 
 CREATE TABLE "Global_Data" (
     "Indicator" VARCHAR(150)   NOT NULL,
-    "Global_Deaths" VARCHAR   NOT NULL,
+    "Global_Deaths" NUMERIC   NOT NULL,
     "Global_Population" NUMERIC   NOT NULL,
     "Indicator_Deaths_%_Total_Deaths" VARCHAR NOT NULL,
     "Indicator_Deaths_%_Total_Population" VARCHAR NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE "Global_Data" (
 CREATE TABLE "Continent_Data" (
     "Continent" VARCHAR(50)   NOT NULL,
     "Indicator" VARCHAR(150)   NOT NULL,
-    "Continent_Deaths" VARCHAR   NOT NULL,
+    "Continent_Deaths" NUMERIC   NOT NULL,
     "Continent_Population" NUMERIC   NOT NULL,
     "Indicator_Deaths_%_Total_Deaths" VARCHAR NOT NULL,
     "Indicator_Deaths_%_Total_Population" VARCHAR NOT NULL
@@ -26,13 +26,11 @@ CREATE TABLE "Country_Data" (
     "Country" VARCHAR(60) NOT NULL,
     "Continent" VARCHAR(50) NOT NULL,
     "Indicator" VARCHAR(150) NOT NULL,
-    "Continent_Deaths" VARCHAR NOT NULL,
-    "Continent_Population" NUMERIC NOT NULL,
+    "Country_Deaths" NUMERIC NOT NULL,
+    "Country_Population" NUMERIC NOT NULL,
     "Indicator_Deaths_%_Total_Deaths" VARCHAR NOT NULL,
     "Indicator_Deaths_%_Total_Population" VARCHAR NOT NULL
 );
-
-
 
 CREATE TABLE "Merged_Data" (
     "Continent_Code" VARCHAR(5)   NOT NULL,
@@ -40,7 +38,7 @@ CREATE TABLE "Merged_Data" (
     "Country_Code" VARCHAR(5)   NOT NULL,
     "Country" VARCHAR(60)   NOT NULL,
     "Indicator_Category" VARCHAR(150)   NOT NULL,
-    "No_Deaths" VARCHAR   NOT NULL,
+    "No_Deaths" NUMERIC   NOT NULL,
     "2016_Population" NUMERIC  NOT NULL
 );
 
@@ -53,3 +51,24 @@ REFERENCES "Global_Data" ("Indicator");
 ALTER TABLE "Merged_Data" ADD CONSTRAINT "fk_Indicator" FOREIGN KEY("Indicator_Category")
 REFERENCES "Global_Data" ("Indicator");
 
+-- Change data type of columns with % values from VARCHAR to DOUBLE and review results
+ALTER TABLE "Global_Data"
+ALTER COLUMN "Global_Deaths" TYPE decimal(13,0) using "Global_Deaths"::decimal,
+ALTER COLUMN "Global_Population" TYPE decimal(13,0) using "Global_Population"::decimal,
+ALTER COLUMN "Indicator_Deaths_%_Total_Deaths" TYPE decimal(5,1) using "Indicator_Deaths_%_Total_Deaths"::decimal,
+ALTER COLUMN "Indicator_Deaths_%_Total_Population" TYPE decimal(5,4)using "Indicator_Deaths_%_Total_Population"::decimal;
+SELECT * FROM "Global_Data"
+
+ALTER TABLE "Continent_Data"
+ALTER COLUMN "Continent_Deaths" TYPE decimal(13,0) using "Continent_Deaths"::decimal,
+ALTER COLUMN "Continent_Population" TYPE decimal(13,0) using "Continent_Population"::decimal,
+ALTER COLUMN "Indicator_Deaths_%_Total_Deaths" TYPE decimal(5,1) using "Indicator_Deaths_%_Total_Deaths"::decimal,
+ALTER COLUMN "Indicator_Deaths_%_Total_Population" TYPE decimal(5,4)using "Indicator_Deaths_%_Total_Population"::decimal;
+SELECT * FROM "Continent_Data"
+
+ALTER TABLE "Country_Data"
+ALTER COLUMN "Country_Deaths" TYPE decimal(13,0) using "Country_Deaths"::decimal,
+ALTER COLUMN "Country_Population" TYPE decimal(13,0) using "Country_Population"::decimal,
+ALTER COLUMN "Indicator_Deaths_%_Total_Deaths" TYPE decimal(5,1) using "Indicator_Deaths_%_Total_Deaths"::decimal,
+ALTER COLUMN "Indicator_Deaths_%_Total_Population" TYPE decimal(5,4)using "Indicator_Deaths_%_Total_Population"::decimal;
+SELECT * FROM "Country_Data"
